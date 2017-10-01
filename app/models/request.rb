bac2ff8efee25ae7e5579a716730bafd806e8e71
Derek DiLeo <derekfdileo@gmail.com>
2017-10-01 11:29:06 -0400

    Add ID and date columns to request index in America/New York time zone

--- app/models/request.rb
+++ app/models/request.rb
@@ -4,6 +4,9 @@ class Request < ApplicationRecord
 	validates :title, length: { minimum: 2 }, allow_blank: true
 	validates :comments, length: { minimum: 2 }, allow_blank: true
 
+	def created_at_in_time_zone
+		created_at.in_time_zone('America/New_York')
+	end
 	private
 
 	def one_field_present
