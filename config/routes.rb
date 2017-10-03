d6b989a78a2383e9a1c27f873eb2e1c28463b592
Derek DiLeo <derekfdileo@gmail.com>
2017-10-03 04:37:29 -0400

    Fix routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
- 	devise_scope :user do
-    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-	end
 	root to: 'requests#new'
   resources :requests
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
