cfba4321ea010608965811bd569e1bcc6664b125
Derek DiLeo <derekfdileo@gmail.com>
2017-10-06 03:33:44 -0400

    Add debugging statement

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,9 +1,16 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
+    p '===================' * 199
+    p '===================' * 199
+    p request.env
+    p '===================' * 199
+
+
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
+
       sign_in_and_redirect new_request_url, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
