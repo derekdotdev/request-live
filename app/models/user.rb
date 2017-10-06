02825b55d84c53a0e4664e546c329a45e1a87f8c
Derek DiLeo <derekfdileo@gmail.com>
2017-10-06 02:46:24 -0400

    Add name and image columns to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ApplicationRecord
   has_many :requests
 
   def self.from_omniauth(auth)
+    p '===================' * 199
+    p auth.info
+    p '===================' * 199
+    
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
