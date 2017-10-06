d6a53a0dd131f5948778139898c68d5246c84963
Derek DiLeo <derekfdileo@gmail.com>
2017-10-06 02:05:41 -0400

    Update config.omniauth and root to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-	root to: 'requests#new'
+	root to: 'requests#index'
   resources :requests
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/thank-you', to: 'requests#thank_you'
