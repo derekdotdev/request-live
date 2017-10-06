9377bc8a4bdeaab02a4d7aaea8b5e378e84d89da
Derek DiLeo <derekfdileo@gmail.com>
2017-10-06 01:27:54 -0400

    Only require Facebook auth on requests/new

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,6 +1,6 @@
 class RequestsController < ApplicationController
   before_action :set_request, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:new]
 
   # GET /requests
   # GET /requests.json
@@ -69,7 +69,7 @@ class RequestsController < ApplicationController
 
   def thank_you
   end
-
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_request
