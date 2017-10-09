d4b948d5c3f6271bfe9880996e5b11f61196cf8d
Derek DiLeo <derekfdileo@gmail.com>
2017-10-09 06:03:21 -0400

    Update sign in and redirect to include @user

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,20 +1,19 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    binding.pry
 
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      sign_in_and_redirect persisted_path, :event => :authentication #this will throw if @user is not activated
+      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook")
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_path 
+      redirect_to @user
     end
   rescue => e
     binding.pry
-    Rails.logger(e)
+
   end
 
   def failure
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-	root to: 'home#welcome'
+	root to: 'requests#new'
   resources :requests
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/thank-you', to: 'requests#thank_you'
