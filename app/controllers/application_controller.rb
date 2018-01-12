ab1fe4abb05524d05a493c5bb7475487e5deca87
Derek DiLeo <derekfdileo@gmail.com>
2018-01-12 04:14:36 -0500

    mend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
   before_action :authenticate_user!
   
   def new_session_path(scope)
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,4 @@
 class HomeController < ApplicationController
-	def redirect_to_https
-    redirect_to :protocol => "https://" unless (request.ssl? || request.local?)
-	end
-
-	before_filter :redirect_to_https
-
 	before_filter :authenticate_user!, except: [:privacy]
 
 	def welcome
