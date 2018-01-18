c441e946cba96300d2cf8180071607768edde0f9
iMears <maxwellmears@gmail.com>
2018-01-17 22:40:55 -0800

    [#154163031] Wire up semantic-ui menu

--- app/assets/javascripts/requests.js
+++ app/assets/javascripts/requests.js
@@ -5,12 +5,12 @@ $(document).ready(function() {
   if (requestCount && onRequestIndexPage) {
     requestPermission();
   }
-  
+
   function handleResponse(result) {
   	if (result !== 'granted') {
   		return alert('please enable notifications');
   	}
-  
+
   	setInterval(checkForRequests, 3000)
   }
 
@@ -22,16 +22,16 @@ $(document).ready(function() {
       success: function(data) {
       	if (data.request_count > requestCount) {
       		requestCount = data.request_count;
-          var notification = data.request.artist + ' - ' + data.request.title
-  		 		new Notification(notification);	
+          var notification = data.request.artist + ' - ' + data.request.title;
+  		 		new Notification(notification);
   		 		window.location.reload();
       	}
       }
-    }); 
+    });
   }
 
   function requestPermission() {
-  	Notification.requestPermission().then(handleResponse);	
+  	Notification.requestPermission().then(handleResponse);
   }
 });
 
@@ -52,10 +52,9 @@ $(document).on('turbolinks:load', function() {
 $(document).ready(function() {
   // Don't run this code if not on the thank you page
   var onThankYouPage = $('.thank-you').length;
-  if (!onThankYouPage) return; 
+  if (!onThankYouPage) return;
 
   $('#follow').click(function(event) {
     $('.social-icons').toggle('fast');
   });
 });
-
