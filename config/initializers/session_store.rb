b4719744a477e4a2298ac5682f6d28d707f1593c
iMears <maxwellmears@gmail.com>
2018-02-01 22:05:48 -0800

    Rename project to Request Live

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_orange-banana_session'
+Rails.application.config.session_store :cookie_store, key: '_request-live_session'
